8a4247646304d7caa078c4050b25fc36169b742e
Ana Maria Tozi <anamariatozi@gmail.com>
2022-03-31 19:29:55 -0300

    Requisito 57 - aplicação do botão de copiar URL

--- src/components/CardDoneRecipes.js
+++ src/components/CardDoneRecipes.js
@@ -1,48 +1,51 @@
 import React from 'react';
+import clipboardCopy from 'clipboard-copy';
 import shareIcon from '../images/shareIcon.svg';
 // import { useSelector } from 'react-redux';
 
 const CardDoneRecipes = () => {
-  
- /*  const handleClick = () => {
-    const copy = require('clipboard-copy')
-  } */
+  const handleClick = () => {
+    clipboardCopy(window.location.href);
+    global.alert('Link copied!');
+  };
 
   return (
-  <>
-    <img
-      src=""
-      alt=""
-      data-testid={ `${index}-horizontal-image` }
-    />
-    <h2
-      data-testid={ `${index}-horizontal-top-text` }
-    >
-      Categoria
-    </h2>
-    <p
-      data-testid={ `${index}-horizontal-name` }
-    >
-      Nome
-    </p>
-    <p
-      data-testid={ `${index}-horizontal-done-date` }
-    />
-    <input
-      data-testid={ `${index}-horizontal-share-btn` }
-      type="image"
-      alt="ícone de compartilhamento"
-      src={ shareIcon }
-      onClick={ handleClick }
-    />
-    <p
-      data-testid={ `${index}-${tagName}-horizontal-tag` }
-    >
-      tags
-    </p>
-    )
-  </>
+    <div>
+      <img
+        src=""
+        alt=""
+        data-testid={ `${index}-horizontal-image` }
+      />
+      <h2
+        data-testid={ `${index}-horizontal-top-text` }
+      >
+        Categoria
+      </h2>
+      <p
+        data-testid={ `${index}-horizontal-name` }
+      >
+        Nome
+      </p>
+      <p
+        data-testid={ `${index}-horizontal-done-date` }
+      >
+        Data
 
+      </p>
+      <input
+        data-testid={ `${index}-horizontal-share-btn` }
+        type="image"
+        alt="share button"
+        src={ shareIcon }
+        onClick={ handleClick }
+      />
+      <p
+        data-testid={ `${index}-${tagName}-horizontal-tag` }
+      >
+        tags
+      </p>
+    </div>
+  );
 };
 
 export default CardDoneRecipes;
