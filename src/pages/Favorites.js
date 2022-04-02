ec34cfd5facb2dc4f553d98f863c75147c8c16a2
Ana Maria Tozi <anamariatozi@gmail.com>
2022-04-02 10:47:59 -0300

    Renderiza botoes na pagina de receitas prontas

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from '../components/Header';
+import ButtonsDoneAndFavorites from '../components/ButtonsDoneAndFavorites';
 
 const Favorites = () => {
   const isVisible = false;
 
   return (
-    <Header
-      label="Favorite Recipes"
-      isVisible={ isVisible }
-    />
+    <>
+      <Header
+        label="Favorite Recipes"
+        isVisible={ isVisible }
+      />
+      <ButtonsDoneAndFavorites />
+    </>
   );
 };
 
