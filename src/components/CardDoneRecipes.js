6f029798d6a83e678eef9506b4c88740c95a6553
Ana Maria Tozi <anamariatozi@gmail.com>
2022-04-03 18:42:14 -0300

    Tela de receitas - configura botoes e card

--- src/components/CardDoneRecipes.js
+++ src/components/CardDoneRecipes.js
@@ -1,104 +1,112 @@
-import React from 'react';
-// import clipboardCopy from 'clipboard-copy';
-import PropTypes, { number, string } from 'prop-types';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import shareIcon from '../images/shareIcon.svg';
-// import { useSelector } from 'react-redux';
+import ButtonsDoneAndFavorites from './ButtonsDoneAndFavorites';
+// import { doneRecipe } from '../services/checkLocalStorageInfo';
 
-const CardDoneRecipes = (infoRecipes) => {
-  // const emailUser = JSON.parse(localStorage.getItem('user'));
-  // const history = useHistory();
+const CardDoneRecipes = () => {
+  const [recipesFromStorage, setRecipesFromStorage] = useState([]);
+  const [recipesToRender, setRecipesToRender] = useState([]);
+  const [isCopied, setCopied] = useState(false);
 
-  // const [localStorage, setLocalStorage] = useLocalStorage('doneRecipes', []);
-  // const [doneRecipes, setDoneRecipes] = useState([]);
-  // const [copied, setCopied] = useState('');
+  useEffect(() => {
+    const getRecipesFromStorage = () => {
+      const doneRecipes = localStorage.getItem('doneRecipes');
+      if (doneRecipes) {
+        const results = JSON.parse(localStorage.getItem('doneRecipes'));
+        setRecipesFromStorage(results);
+        setRecipesToRender(results);
+      }
+    };
+    getRecipesFromStorage();
+  }, []);
 
-  // const doneRecipeStorage = JSON.parse(localStorage.getItem('doneRecipes'));
+  const handleFilter = ({ target }) => {
+    const { value } = target;
 
-  const handleClick = () => {
-    navigator.clipboard.writeText(window.location.href) // Source: https://www.w3schools.com/howto/howto_js_copy_clipboard.asp;
-      .then(global.alert('Link copied!'));
+    if (value === 'food') {
+      const filteredByFood = recipesFromStorage
+        .filter((recipe) => (recipe.type === 'food'));
+      setRecipesToRender(filteredByFood);
+    }
+
+    if (value === 'drink') {
+      const filteredByDrink = recipesFromStorage
+        .filter((recipe) => (recipe.type === 'drink'));
+      setRecipesToRender(filteredByDrink);
+    }
+
+    if (value === 'all') {
+      setRecipesToRender(recipesFromStorage);
+    }
   };
 
-  const { index, type } = infoRecipes;
-  let page;
-  let name;
-  let img;
-  if (type === 'foods') {
-    const { idMeal, strMeal, strMealThumb } = infoRecipes;
-    page = `/foods/${idMeal}`;
-    name = strMeal;
-    img = strMealThumb;
+  function handleClick() {
+    const url = window.location.href;
+    const newUrl = url.replace('/in-progress', '');
+    navigator.clipboard.writeText(newUrl);
+    setCopied(true);
   }
 
-  if (type === 'drinks') {
-    const { idDrink, strDrink, strDrinkThumb } = infoRecipes;
-    page = `/drinks/${idDrink}`;
-    name = strDrink;
-    img = strDrinkThumb;
-  }
   return (
-    /* { doneRecipeStorage.map((recipe, index) => ( */
     <div>
-      <Link
-        data-testid={ `${index}-recipe-card` }
-        to={ page }
-      >
-        <img
-          src={ recipe.image }
-          alt="done recipe image"
-          data-testid={ `${index}-horizontal-image` }
-        />
-
-        <p
-          data-testid={ `${index}-horizontal-name` }
-        >
-          {recipe.name}
-        </p>
-      </Link>
-
-      <h2
-        data-testid={ `${index}-horizontal-top-text` }
-      >
-        {recipe.category}
-      </h2>
-      <p
-        data-testid={ `${index}-horizontal-done-date` }
-      >
-        {recipe.doneDate}
-
-      </p>
-
-      {recipe.type === 'food' ? (recipe.tags.map((tagName) => (
-        <p
-          data-testid={ `${index}-${recipe.tags}-horizontal-tag` }
-        >
-          {tagName}
-        </p>
-      ))) : ''}
-      {' '}
-      */
-
-      <input
-        data-testid={ `${index}-horizontal-share-btn` }
-        type="image"
-        alt="share button"
-        src={ shareIcon }
-        onClick={ handleClick }
+      <ButtonsDoneAndFavorites
+        handleFilter={ handleFilter }
       />
+      {recipesToRender.map((recipe, index) => (
+        <div key={ index }>
+          <Link
+            data-testid={ `${index}-recipe-card` }
+            to={ `/${recipe.type}/${recipe.id}` }
+          >
+            <img
+              src={ recipe.image }
+              alt="done recipe"
+              data-testid={ `${index}-horizontal-image` }
+            />
 
+            <p
+              data-testid={ `${index}-horizontal-name` }
+            >
+              {recipe.name}
+            </p>
+          </Link>
+
+          <p
+            data-testid={ `${index}-horizontal-done-date` }
+          >
+            {recipe.doneDate}
+
+          </p>
+
+          <input
+            data-testid={ `${index}-horizontal-share-btn` }
+            type="image"
+            alt="share button"
+            src={ shareIcon }
+            onClick={ handleClick }
+          />
+          {isCopied ? <p>Link copied</p> : false}
+
+          {/* se a receita é uma comida, renderiza a categoria, nacionalidade,
+
+          se não, renderiza se é alcooliza ou não */}
+
+          <p data-testid={ `${index}-horizontal-top-text` }>
+            {recipe.type === 'food'
+              ? `${recipe.nationality} - ${recipe.category}` : `${recipe.alcoholicOrNot}`}
+          </p>
+
+          {/* se a receita for comida, renderiza as tags */}
+          {recipe.type === 'food'
+            ? (recipe.tags.map((tagName) => (
+              <p data-testid={ `${index}-${tagName}-horizontal-tag` }>
+                {tagName}
+              </p>))) : ''}
+
+        </div>
+      ))}
     </div>
-    /*  ))}; */
-
   );
-  CardDoneRecipes.propTypes = {
-    infoRecipes: PropTypes.shape({
-      index: number.isRequired,
-      idMeal: number.isRequired,
-      strMeal: string.isRequired,
-      strMealThumb: string.isRequired,
-    }).isRequired,
-  };
 };
 
 export default CardDoneRecipes;
